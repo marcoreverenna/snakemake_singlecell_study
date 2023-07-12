3861423c1bad0a61258ec0e29317415ce18b958b
Marco Reverenna <89908852+marcoreverenna@users.noreply.github.com>
2023-07-12 13:50:43 +0200

    Processing parquet files

---
+++ processing_parquet.py
@@ -0,0 +1,22 @@
+from src import snparray_to_vcf_mod as snplib
+from seqseek import Chromosome, BUILD37
+import os
+import pandas as pd
+
+list_families = ['PGD036', 'PGD043']
+dir = '../imputation_pipeline/data/raw'
+
+if __name__=='__main__':
+    for family in list_families:
+        vcf_ref= snplib.import_vcf_reference()
+        vcf_ref['REFALT_DBSNP']=vcf_ref['REF']+vcf_ref['ALT_1']
+        vcf_ref=vcf_ref.rename(columns={'ID':'Name'})[['REFALT_DBSNP','Name']]
+        to_refalt=snplib.load_manifest('../imputation_pipeline/data/raw/HumanCytoSNP-12v2-1_hg19.bpm')    
+        df = pd.read_parquet(f'../imputation_pipeline/data/raw/{family}_stage2_nofilter_hg19.df.parquet',engine='pyarrow')
+        df=df.set_index('Name').join(to_refalt.set_index('Name'))
+        df.rename(columns={'variant/REFALT':'REFALT_MANIFEST'},inplace=True)
+        df=df.join(vcf_ref.set_index('Name'))
+        df['gtype_vcf']=df.apply(lambda x: snplib.map_ab_to_bin(x['REFALT_MANIFEST'],x['REFALT_DBSNP'],x['gtype'],x['Chr'],x['Position'],BUILD37),axis=1)
+        df['gtype_reconstructed']=df.apply(lambda x: snplib.map_bin_to_ab(x['REFALT_MANIFEST'],x['REFALT_DBSNP'],x['gtype_vcf'],x['Chr'],x['Position'],BUILD37),axis=1)
+        parq = df.reset_index().copy()
+        parq.to_parquet(os.path.join(dir, f'{family}_stage2_nofilter_hg19_processed.parquet'), engine='pyarrow')
